45dd620979ae2a4d8c3596a03a2511e203bd551d
Steffen Geipel <geipel.steffen@gmail.com>
2020-12-22 20:57:07 +0100

    landingpage

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,25 +1,24 @@
 import Link from 'next/link'
-
-const links = [
-  { href: 'https://github.com/vercel/next.js', label: 'GitHub' },
-  { href: 'https://nextjs.org/docs', label: 'Docs' },
-]
+import SvgLogo from '@svg/Logo_small.svg'
 
 export default function Nav() {
   return (
     <nav>
       <ul className='flex items-center justify-between p-8'>
         <li>
-          <img src='Logo_small.png'></img>
+          <SvgLogo></SvgLogo>
         </li>
         <ul className='flex items-center justify-between space-x-4'>
-          {links.map(({ href, label }) => (
-            <li key={`${href}${label}`}>
-              <a href={href} className='no-underline btn-blue'>
-                {label}
-              </a>
-            </li>
-          ))}
+          <li key={`LogIn`}>
+            <button className='no-underline bg-white py-4 px-4 text-blue rounded-lg w-24'>
+              Log In
+            </button>
+          </li>
+          <li key={`SingUp`}>
+            <button className='no-underline bg-blue-700 py-4 px-4 text-white rounded-lg w-24'>
+              Sing Up
+            </button>
+          </li>
         </ul>
       </ul>
     </nav>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Nav from '../components/nav'
+import SvgTime from '@svg/time.svg'
 
 // Landingpage
 export default function IndexPage() {
@@ -9,15 +10,11 @@ export default function IndexPage() {
         <h1 className='text-5xl text-center text-gray-700 dark:text-gray-100'>
           Next.js + Tailwind CSS 2.0
         </h1>
+        <SvgTime></SvgTime>
       </div>
       <style jsx>{`
         #maindiv {
-          background-color: #03103b;
-          background-image: linear-gradient(
-            135deg,
-            rgba(64, 219, 216, 0.15) 0%,
-            rgba(3, 16, 59, 0.15) 35%
-          );
+          background-color: rgb(233, 239, 248);
         }
       `}</style>
     </div>
