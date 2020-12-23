b47fcb94b6d1b559d8fffefb015666371b3860dd
Steffen Geipel <geipel.steffen@gmail.com>
2020-12-23 13:12:39 +0100

    working build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
-const colors = require('tailwindcss/colors')
-
 module.exports = {
   purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'media', // 'media' or 'class'
   theme: {
     extend: {
       colors: {
-       lightBlue: colors.lightBlue
+        'accent-1': '#333',
       },
     },
   },
